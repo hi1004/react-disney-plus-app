ba4a86eab7cbd94d144324ce3f7eaff55c89605f
hi1004 <kipo09@naver.com>
2023-03-09 02:28:18 +0900

    video banner container 생성 및 스타일링

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from './components/Nav';
+import HomeBackground from './assets/images/home-background.png';
 
 const App = () => {
   return (
     <div className="App">
-      <Nav />
+      <Container>
+        <Nav />
+      </Container>
     </div>
   );
 };
 
 export default App;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    background: url(${HomeBackground}) center / cover no-repeat;
+    content: '';
+    position: absolute;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
