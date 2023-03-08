64b8a0b8a589eef87bb5040be2bd9250a29d87c8
hi1004 <kipo09@naver.com>
2023-03-09 01:44:52 +0900

    chore: arrow function rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,9 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
   },
 };
