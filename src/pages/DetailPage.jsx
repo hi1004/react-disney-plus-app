f38832feb9f3c618797662ab6c018d0623f20dc4
hi1004 <kipo09@naver.com>
2023-03-12 21:54:02 +0900

    feat: detail page 상세 영화 정보(제목, 년도, 포스터) 데이터 불러오고 이미지 404 error 구현

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from '../api/axios';
+import error404 from '../assets/images/404_error.png';
 
 const DetailPage = () => {
-  return <div />;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get(`/movie/${movieId}`);
+        setMovie(response.data);
+      } catch (err) {
+        setError(true);
+        throw new Error(err);
+      }
+    };
+    fetchMovie();
+  }, [movieId]);
+
+  return (
+    <MovieDetail>
+      <div className="movie-contents">
+        <h2>{movie.title || movie.original_title}</h2>
+        <p>{movie?.release_date}</p>
+        {!error && movie.backdrop_path !== undefined && (
+          <img
+            src={
+              movie?.backdrop_path !== null
+                ? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
+                : error404
+            }
+            alt={movie.title}
+          />
+        )}
+        {error && <div className="error" />}
+      </div>
+    </MovieDetail>
+  );
 };
 
 export default DetailPage;
+
+const MovieDetail = styled.section`
+  margin-top: 100px;
+  .movie-contents {
+    text-align: center;
+    img {
+      max-width: 1000px;
+      display: block;
+      margin: 30px auto;
+    }
+    .error {
+      max-width: 1000px;
+      height: 1033px;
+      background: url(${error404});
+      background-size: cover;
+      background-repeat: no-repeat;
+      margin: 0 auto;
+    }
+  }
+  h2 {
+    font-size: 2rem;
+  }
+`;
