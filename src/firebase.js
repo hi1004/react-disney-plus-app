f1f6849cce54b0cbb9ffacdb69f385d73b11b93f
hi1004 <kipo09@naver.com>
2023-03-15 23:16:21 +0900

    fixed: firebase api키 데이터 복구

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: 'AIzaSyAGZsTl5g9atq0MYXvF7TaS2igLNDWR9NQ',
+  authDomain: 'react-disney-plus-app-e1dbc.firebaseapp.com',
+  projectId: 'react-disney-plus-app-e1dbc',
+  storageBucket: 'react-disney-plus-app-e1dbc.appspot.com',
+  messagingSenderId: '795267705736',
+  appId: '1:795267705736:web:40ea89937d58f791efa83d',
 };
 
 const app = initializeApp(firebaseConfig);
